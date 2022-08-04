73aa36c5c640d810dc8a26ddd50f790b15a96f3c
jtaraya <jacobtaraya@gmail.com>
2022-08-04 22:23:58 +0300

    updated gradle and models

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -3,14 +3,26 @@ package models;
 public class Department {
     private String name;
     private String description;
+    private int employeeCount;
     private int id;
-    private long created;
-    private long updated;
-    private String deleted;
+//    private static ArrayList<Department> instances = new ArrayList<>();
 
-    public Department(String name, String description){
+    public Department(){}
+
+
+    public Department(String name) {
         this.name = name;
-        this.description = description;
+    }
+
+    public Department(String name, String description) {
+        this.name = name;
+        this.description= description;
+//        this.createdAt = LocalDateTime.now();
+    }
+    public Department(String name, String description,int employeeCount) {
+        this.name = name;
+        this.description= description;
+        this.employeeCount=employeeCount;
     }
 
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -2,24 +2,21 @@ package models;
 
 public class User {
     private int id;
-    private String first_name;
-    private String last_name;
-    private String staff_id;
-    private String user_position;
-    private String phone_no;
-    private String email;
-    private String photo;
-    private long created;
-    private long updated;
-    private String deleted;
+    private String staffName;
+    private String staffPosition;
+    private String staffRole;
+    private int departmentId;
 
-    public User(String firstName, String lastName, String staffId, String position, String phoneNo, String email, String photo){
-        this.first_name = firstName;
-        this.last_name = lastName;
-        this.staff_id = staffId;
-        this.user_position = position;
-        this.phone_no = phoneNo;
-        this.photo = photo;
+    public User(){}
+
+    public User(String staffName) {
+        this.staffName = staffName;
+    }
+
+    public User(String staffName, String staffPosition, String staffRole, int departmentId ) {
+        this.staffName = staffName;
+        this.staffPosition= staffPosition;
+        this.staffRole = staffRole;
     }
 
 
