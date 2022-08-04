6137e3aed11479b2591f32e12940e16c9209f60b
jtaraya <jacobtaraya@gmail.com>
2022-08-04 13:01:53 +0300

    added Department Class

---
+++ src/main/java/models/Department.java
@@ -0,0 +1,10 @@
+package models;
+
+public class Department {
+    private String name;
+    private String description;
+    private int id;
+    private long created;
+    private long updated;
+    private String deleted;
+}
