bf3bed1ae3edeb6163b4bd27c1e637eb6643f479
jtaraya <jacobtaraya@gmail.com>
2022-08-04 12:42:50 +0300

    added User Class

---
+++ src/main/java/models/User.java
@@ -0,0 +1,15 @@
+package models;
+
+public class User {
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String staff_id;
+    private String user_position;
+    private String phone_no;
+    private String email;
+    private String photo;
+    private long created;
+    private long updated;
+    private String deleted;
+}
