35dee651d312d71588e135c3eca3295e622f833b
jtaraya <jacobtaraya@gmail.com>
2022-08-05 08:01:53 +0300

    updated Constants class

--- src/main/java/dao/Constants.java
+++ src/main/java/dao/Constants.java
@@ -1,4 +1,8 @@
 package dao;
 
 public class Constants {
+    public static final String USERNAME="jtaraya";
+    public static final String PASSWORD="postgres";
+    //    public static final String URL_TEST="postgres://localhost:5432/Staff-App-test";
+    public static final String URL_PROD="postgres://localhost:5432/Staff-App";
 }
